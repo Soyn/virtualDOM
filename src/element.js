cc6df9e0410e68681a5658e667eeec359200e879
Soyn <w604408482@163.com>
2018-08-15 23:45:46 +0800

    feat: add children check, children counts support

--- src/element.js
+++ src/element.js
@@ -1,8 +1,29 @@
+import _ from 'lodash';
 class Element {
   constructor(tagName, props, children) {
+    if (!_.isArray(children) && children != null) {
+      children = _.slice(arguments, 2).filter(child => !!child);
+    }
+
+    if(_.isArray(props)) {
+      children = props;
+      props = {}
+    }
     this.tagName = tagName;
-    this.props = props;
-    this.children = children;
+    this.props = props || {};
+    this.children = children || [];
+    this.key = props ? props.key : void 0;
+
+    let count = 0;
+    _.each(this.children, (child, i) => {
+      if (child instanceof Element) {
+        count += child.count;
+      } else {
+        children[i] = '' + child;
+      }
+      count++;
+    });
+    this.count = count;
   }
 }
 
