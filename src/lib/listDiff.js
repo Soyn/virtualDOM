88e4476d8ab7857988588df9618a641ca99cd0d1
Soyn <w604408482@163.com>
2018-08-20 00:05:22 +0800

    feat: add list diff algorithm implementation

--- src/lib/listDiff.js
+++ src/lib/listDiff.js
@@ -3,6 +3,7 @@
  * More details in https://reactjs.org/docs/reconciliation.html
  * https://github.com/Matt-Esch/virtual-dom/blob/master/vtree/diff.js
  */
+import { REMOVE_ELEMENT, INSERT_ELEMENT} from './VIrtualDomSymbols'
 function diff(oldList, newList, key) {
   const oldMap = makeKeyIndexAndFree(oldList, key);
   const newMap = makeKeyIndexAndFree(newList, key);
@@ -15,6 +16,8 @@ function diff(oldList, newList, key) {
   const children = [];
   let item, itemKey;
   let freeIndex = 0;
+
+  // get the common item between `newlist` and `oldlist`
   for (let i = 0; i < oldList.length; i++) {
     item = oldList[i];
     itemKey = getItemKey(item, key);
@@ -30,9 +33,66 @@ function diff(oldList, newList, key) {
       children.push(freeItem);
     }
   }
-  const remove = (i) => {
-    
+  const moves = [];
+  const simulateList = children.slice(0);
+  for(let i = 0; i < simulateList.length; i++) {
+    if(simulateList[i] === null) {
+      remove(i);
+      removeSimulate(i);
+    }
   }
+  let i = j = 0;
+  for (i = 0; i < newList.length; i++) {
+    item = newList[i];
+    itemKey = getItemKey(item, key);
+
+    const simulateItem = simulateList[j];
+    const simulateItemKey = getItemKey(simulateItem, key);
+
+    if (simulateItem) {
+      if (itemKey === simulateItemKey) {
+        j++;
+      } else {
+        // insert new element
+        if (!oldKeyIndex.hasOwnProperty(itemKey)) {
+          insert(i, item);
+        } else {
+          const nextItemKey = getItemKey(simulateList[j + 1], key);
+          if(nextItemKey === itemKey) {
+            remove(i);
+            removeSimulate(j);
+            j++;
+          } else {
+            insert(i, item);
+          }
+        }
+      }
+    } else {
+      insert(i, item);
+    }
+  }
+
+  let k = simulateList.length - j;
+  while (j < simulateList.length) {
+    k--;
+    remove(k + i);
+    j++;
+  }
+  const remove = (index) => {
+    const move = {index: index, type: REMOVE_ELEMENT };
+    moves.push(move);
+  }
+  const insert = (index, item) => {
+    const move = { index, item, type: INSERT_ELEMENT };
+    moves.push(move);
+  }
+  const removeSimulate = (index) => {
+    simulateList.splice(index, 1);
+  }
+  return {
+    moves,
+    children
+  };
 }
 function makeKeyIndexAndFree(list, key) {
   const keyIndex = {};
